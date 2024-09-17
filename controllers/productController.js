85793fb403e2902e56768a3e5e06b58b16e0ac40
OG-1534 <gershomochanda@gmail.com>
2024-09-17 12:21:38 +0300

    update products function and routing

--- controllers/productController.js
+++ controllers/productController.js
@@ -21,4 +21,23 @@ const returnProducts = async(req,res) => {
         }
 };
 
-module.exports = { createProduct, returnProducts };
+// UPDATE PRODUCT
+const updateProduct = async(req,res) => {
+	try {
+		const id = req.params.id;
+		const productExist = await ProductModel.findOne({ _id: id });
+
+		if(!productExist){
+			return res.status(404).json({ message: "Product does not exist" });
+		}
+
+		const result = await ProductModel.findByIdAndUpdate(id, req.body, {
+			new: true,
+		});
+		res.status(200).json(result);
+	}       catch(err) {
+		res.status(500).json(err);
+	}
+};
+
+module.exports = { createProduct, returnProducts, updateProduct };
