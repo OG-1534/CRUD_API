f2fe9155430d42a345d95eae7c4d60775a52441a
OG-1534 <gershomochanda@gmail.com>
2024-09-17 12:21:24 +0300

    update products function and routing

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,9 +1,14 @@
 const express = require("express");
-const {createProduct, returnProducts} = require("../controllers/productController");
+const {
+	createProduct,
+	returnProducts,
+	updateProduct
+} = require("../controllers/productController");
 
 const router = express.Router();
 
 router.post("/create", createProduct);
 router.get("/read", returnProducts);
+router.put("/update/:id", updateProduct);
 
 module.exports = router;
