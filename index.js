d7d509d663f935741efcf4d8e09899b26d4411a9
OG-1534 <gershomochanda@gmail.com>
2024-09-18 19:34:23 +0300

    handling string interpolation for port value

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const db = process.env.DBURI;
 mongoose.connect(db).then(() => {
 	console.log("Connected to DB");
 	app.listen(PORT, () => {
-		console.log('Server is running on ${PORT}');
+		console.log(`Server running on port ${PORT}`);
 	});
 })
 .catch((err) => {
